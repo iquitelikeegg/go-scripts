80d597f466544519f9d5761d39b873e93caad60e
Vincent Chan <vincent@digitalfineprint.com>
2019-11-11 11:52:56 +0000

    upload to s3 file

--- send-files-to-s3/main.go
+++ send-files-to-s3/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	S3Region         = "eu-west-2"
+	S3Bucket         = "dfp-datalake-london"
+	bucketBasePath   = "dfp/raw/crime/data.police.uk"
+	defaultOutputDir = "~/Projects/go-scripts-output"
 )
 
 func main() {
@@ -14,6 +28,13 @@ func main() {
 
 	if len(os.Args) >= 2 {
 		sourceDirPath = os.Args[1]
+	}
+
+	outputPath := defaultOutputDir
+
+	// The 3rd output is the output directory
+	if len(os.Args) >= 3 {
+		outputPath = os.Args[2]
 	}
 
 	dateDirectories, err := ioutil.ReadDir(sourceDirPath)
@@ -54,16 +75,53 @@ func main() {
 		fmt.Print(monthFiles)
 		fmt.Print("\n\nzipping...\n")
 
-		monthZip := fmt.Sprintf("%s.zip", dateDirectory.Name())
+		YearDirName, fileName := GenerateFileName(dateDirectory.Name())
+		outputFile := fmt.Sprintf("%s/%s/%s.zip", outputPath, YearDirName, fileName)
 
-		if err := ZipFiles(monthZip, monthFiles); err != nil {
+		if err := ZipFiles(outputFile, monthFiles); err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Print(monthZip)
+		log.Print(fmt.Sprintf("Uploading file to S3 %s", outputFile))
+
+		UploadToS3(outputFile, fmt.Sprintf("%s/%s.zip", YearDirName, fileName))
 
 		break
 	}
+}
+
+func UploadToS3(fileDir string, uploadedFileName string) error {
+	s, err := session.NewSession(&aws.Config{Region: aws.String(S3Region)})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open(fileDir)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	// Get file size and read the file content into a buffer
+	fileInfo, _ := file.Stat()
+	var size int64 = fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+
+	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(S3Bucket),
+		Key:                  aws.String(fmt.Sprintf("%s/%s", bucketBasePath, uploadedFileName)),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+	})
+
+	return err
 }
 
 func ZipFiles(filename string, files []string) error {
@@ -119,4 +177,10 @@ func AddFileToZip(zipWriter *zip.Writer, filename string) error {
 	}
 	_, err = io.Copy(writer, fileToZip)
 	return err
+}
+
+// GenerateFileName - Generate a File name from a directory of format YYYY/MM eg. 2019-09
+func GenerateFileName(directoryName string) (string, string) {
+	pieces := strings.Split("-", directoryName)
+	return pieces[0], pieces[1]
 }
